5fe5b5677b0241a0233a0b7dde2708a010df238b
Steve Grant <tricalendars@gmail.com>
2026-05-11 03:33:29 -0500

    Fix icon paths and add constants to repo

--- app.config.js
+++ app.config.js
@@ -5,9 +5,20 @@ export default {
     name: "Tell ME",
     slug: "tellme-native",
     version: "1.0.0",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "dark",
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#0F0D0B",
+    },
     android: {
       package: "com.tellmeapp.tellme",
       versionCode: 1,
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#0F0D0B",
+      },
     },
     extra: {
       anthropicKey: process.env.ANTHROPIC_KEY,
